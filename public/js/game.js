129e7b6c5e6994c9090fc4e5d5af7a6c316699c6
Adam Toczko <adamtoczko80@gmail.com>
2019-12-20 11:39:27 +0100

    added typpe modules in script index html started creation of MainScene

--- public/js/game.js
+++ public/js/game.js
@@ -1,9 +1,12 @@
+import MainScene from "./MainScene.js";
+
+
 const config = {
     width: 640,
     height: 640,
     type: Phaser.AUTO, // will auto-detect, Phaser.WEBGL, Phaser.CANVAS or Phaser.HEADLESS (no rendering at all).
     parent: 'phaser-game',//The DOM element into which this games canvas will be injected
-    scene: []
+    scene: [MainScene]
 }
 
 new Phaser.Game(config);
