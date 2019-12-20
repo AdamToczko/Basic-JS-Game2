310142e6d50916dcd0d73bce6bb3302bc8888269
Adam Toczko <adamtoczko80@gmail.com>
2019-12-20 11:16:21 +0100

    created Phaser board in index.js addedd path for express to serve files

--- public/js/game.js
+++ public/js/game.js
@@ -1,8 +1,8 @@
 const config = {
     width: 640,
     height: 640,
-    type: Phaser.AUTO,
-    parent: 'phaser-game',
+    type: Phaser.AUTO, // will auto-detect, Phaser.WEBGL, Phaser.CANVAS or Phaser.HEADLESS (no rendering at all).
+    parent: 'phaser-game',//The DOM element into which this games canvas will be injected
     scene: []
 }
 
