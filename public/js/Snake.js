1611b0f79084a53cb41951f815a0661592f6c850
Adam Toczko <adamtoczko80@gmail.com>
2019-12-20 13:07:50 +0100

    testing basic movement

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,8 +1,12 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene; // creating and assignin scene property 
-        this.scene.add.rectangle(10,10,16,16,0xff0000)
+        this.body =[]; //snake will grow
+        this.body.push(this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0));
     }
 
-    update(time) {}
+    update(time) {
+        this.body[0].x += 1;
+        this.body[0].y += 1;
+    }
 } 
