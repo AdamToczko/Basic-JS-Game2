b146781308d5a885d8fdf6ebb0e1f4cb8ea63cbb
Adam Toczko <adamtoczko80@gmail.com>
2019-12-20 16:47:37 +0100

    added 3rd segemnt of snake and function for movement of each new segment

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -7,8 +7,9 @@ export default class Snake {
 
         this.direction = Phaser.Math.Vector2.RIGHT; // start movement direction
         this.body =[]; //snake will grow
-        this.body.push(this.scene.add.rectangle(0,0,16,16,0xff0000).setOrigin(0)); // head
-        this.body.push(this.scene.add.rectangle(0,0,16,16,0x0000ff).setOrigin(0)); //body
+        this.body.push(this.scene.add.rectangle(100,100,16,16,0xff0000).setOrigin(0)); // head
+        this.body.push(this.scene.add.rectangle(0,0,16,16,0x0000ff).setOrigin(0)); //body1
+        this.body.push(this.scene.add.rectangle(0,0,16,16,0xffffff).setOrigin(0)); //body2
 
         scene.input.keyboard.on('keydown', e => {this.keydown(e)}) // key press detection
     }
@@ -38,10 +39,13 @@ export default class Snake {
     }
 
     move() {
-        this.body[1].x = this.body[0].x; // attach tail to body works with 2 segments 
-        this.body[1].y = this.body[0].y;
-        this.body[0].x += this.direction.x *10;
+        // as ther will be more segments in for loop we will attach each new segment to one before 
+        for(let index = this.body.length - 1; index > 0; index --){
+            this.body[index].x = this.body[index-1].x; 
+            this.body[index].y = this.body[index-1].y;
+        }
+        this.body[0].x += this.direction.x *10; 
         this.body[0].y += this.direction.y *10;
     }
-
+    
 } 
